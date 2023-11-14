4405e1112c1499bf68601d794202c2684ca2ebd5
Alexander Nguyen <altecx.nguyen@gmail.com>
2023-11-14 18:34:47 -0500

    Exercises

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -4,7 +4,6 @@ import { Stack, Typography } from "@mui/material";
 import Icon from "../assets/icons/gym.png";
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
-  console.log(bodyPart);
   return (
     <Stack
       type="button"
@@ -20,12 +19,24 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         cursor: "pointer",
         gap: "47px",
       }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      }}
     >
       <img
         src={Icon}
         alt="dumbbell"
         style={{ width: "40px", height: "40px" }}
       />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 };
